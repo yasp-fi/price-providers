db806a998b4de0cfbce604a14924a7f1e4d97535
Георгий Молчанов <wertusser@gmail.com>
2023-01-08 20:33:04 +0100

    feat: thegraph integration

---
+++ src/providers/thegraph/constants.ts
@@ -0,0 +1,1 @@
+export const THE_GRAPH_QUERY_API = `https://api.thegraph.com`
---
+++ src/providers/thegraph/types.ts
@@ -0,0 +1,17 @@
+import { PriceQuoteType } from '@yasp/models'
+
+export type UniswapToken = {
+	id: string
+  symbol: string
+  decimals: string
+  derivedETH: string
+}
+
+export type UniswapTokenQueryResponse = {
+  data: {
+    bundle: {
+      ethPriceUSD: string
+    }
+    tokens: UniswapToken[]
+  }
+}
